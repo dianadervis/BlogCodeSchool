45f9a99a43b134911fe402e573ba70a130325a2f
dianadervis <92575364+dianadervis@users.noreply.github.com>
2023-04-12 12:37:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function openNewPageArticles() {
+
+    var url = 'index.html';
+    
+
+    window.location.href = url;
+  }
+  function openNewPageReviews() {
+
+    var url = 'reviews.html';
+    
+
+    window.location.href = url;
+  }
+  function openNewPageContact() {
+
+    var url = 'contact.html';
+    
+
+    window.location.href = url;
+  }
